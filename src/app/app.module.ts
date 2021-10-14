3628d427bdd1da64b200927112ec8435b1a1b01f
David Cediel <esdago18@gmail.com>
2021-10-14 16:04:22 -0500

    Finished Heroes component and starting hero detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
-    FormsModule
+    FormsModule, 
+    BrowserAnimationsModule, 
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
